18486358d57bfc51cd868c5cb37628925f32184c
Swair Shah <swairshah@gmail.com>
2017-06-11 17:40:52 -0500

    minor changes in ipy

--- ipy.py
+++ ipy.py
@@ -1,5 +1,13 @@
 from gram_schmidt import *
 from matrix_stuff import *
+from conjugate_gs import *
+from laplacian import *
 
 A = np.random.rand(10, 15)
 B = np.random.rand(4, 5)
+
+G = nx.complete_graph(4)
+L = Laplacian(G)
+E = IncidenceMatrix(G)
+L_plus = np.linalg.pinv(L)
+
